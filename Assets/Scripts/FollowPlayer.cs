605329a3ea342c00c1714f8188607c6f8e1bca37
Snowspace <snxwspace@gmail.com>
2025-04-21 12:52:37 -0700

    added basic camera trailing

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    private Vector3 offsetTarget = new Vector3(0, 4, -9);
+    public GameObject following;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,8 +11,9 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        // I want the camera movement to be smooth, but idrk how to do that effectively
+        transform.position = following.transform.position + offsetTarget;
     }
 }
