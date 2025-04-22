5a53e83fb393dccd0d6fff314781235de6034b56
Snowspace <snxwspace@gmail.com>
2025-04-22 15:11:05 -0700

    added enemy's random walk effect

--- Assets/Scripts/RandomWalk.cs
+++ Assets/Scripts/RandomWalk.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class RandomWalk : MonoBehaviour
 {
+    private Vector3 direction;
+    public float minimumDirectionTime = 4.0f;
+    public float maximumDirectionTime = 7.0f;
+    // private float timeElapsed;
+    private float randomTime;
+    public float speed = 5.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Invoke("ChangeDirection", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(direction * speed * Time.deltaTime);
     }
+
+    void ChangeDirection() {
+        direction = new Vector3(Random.Range(-1, 2), 0, Random.Range(-1, 2));
+        direction.Normalize();
+        
+        // Recall this function after a time has passed
+        randomTime = Random.Range(minimumDirectionTime, maximumDirectionTime);
+        Invoke("ChangeDirection", randomTime);
+    } 
 }
