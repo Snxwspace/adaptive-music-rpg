dba9621fcf244dc61d15bd8cf6aae2fef2f0c31c
Snowspace <snxwspace@gmail.com>
2025-04-23 10:50:13 -0700

    added rudimentary jump system; added normalization for the player movement

--- Assets/Scripts/PlayerControllerMain.cs
+++ Assets/Scripts/PlayerControllerMain.cs
@@ -1,17 +1,19 @@
+using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
 public class PlayerControllerMain : MonoBehaviour
 {
     private float horizontalInput;
     private float verticalInput;
     public float speed = 10.0f;
-    public float jumpSpeed = 10.0f;
+    private Vector3 jumpForce = new Vector3(0, 10, 0);
+    private Rigidbody selfRigidbody;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // grabbing rigidbody from self
+        selfRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,12 +22,32 @@ public class PlayerControllerMain : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         
-        // consider making the speed stay equalized for whatever angle you're travelling at
-        // how though...? 
-        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * Time.deltaTime * speed);
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        direction = normalizeVector(direction);
+
+        transform.Translate(direction * Time.deltaTime * speed);
+
 
         if(Input.GetKeyDown(KeyCode.Space)) { // fix midair jumping
-            // add velocity
+            Vector3 velocity = selfRigidbody.GetRelativePointVelocity(selfRigidbody.centerOfMass); // gets rigidbody velocity at the center of mass
+            selfRigidbody.AddForce(jumpForce - velocity, ForceMode.VelocityChange); // adds a force equal to and exceeding the current velocity
         }
+    }
+
+    Vector3 normalizeVector(Vector3 direction) {
+        float xFactor = 1.0f;
+        float zFactor = 1.0f;
+        if(direction.x != 0) {
+            xFactor = math.abs(direction.x);
+        }
+        if(direction.z != 0) {
+            zFactor = math.abs(direction.z);
+        }
+        direction.x /= xFactor;
+        direction.z /= zFactor;
+        direction.Normalize();
+        direction.x *= xFactor;
+        direction.z *= zFactor;
+        return direction;
     }
 }
