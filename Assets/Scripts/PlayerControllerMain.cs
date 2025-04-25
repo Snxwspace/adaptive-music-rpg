10bf99b39effd952383a210000a9cf68ca4c9666
Snowspace <snxwspace@gmail.com>
2025-04-25 10:36:19 -0700

    added extra gravity for the player character

--- Assets/Scripts/PlayerControllerMain.cs
+++ Assets/Scripts/PlayerControllerMain.cs
@@ -6,8 +6,9 @@ public class PlayerControllerMain : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
     public float speed = 10.0f;
-    private Vector3 jumpForce = new Vector3(0, 10, 0);
+    private Vector3 jumpForce = new Vector3(0, 15, 0);
     private Rigidbody selfRigidbody;
+    public float gravityScale = 2.0f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -32,6 +33,12 @@ public class PlayerControllerMain : MonoBehaviour
             Vector3 velocity = selfRigidbody.GetRelativePointVelocity(selfRigidbody.centerOfMass); // gets rigidbody velocity at the center of mass
             selfRigidbody.AddForce(jumpForce - velocity, ForceMode.VelocityChange); // adds a force equal to and exceeding the current velocity
         }
+    }
+
+    // FixedUpdate is like update but for physics calculations
+    void FixedUpdate()
+    {
+        selfRigidbody.AddForce(Physics.gravity * (gravityScale-1) * selfRigidbody.mass);
     }
 
     Vector3 normalizeVector(Vector3 direction) {
